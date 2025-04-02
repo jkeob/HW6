e9dbcd35963ab0784717e15aab121575778f8a53
Jacob Oh <97423147+jkeob@users.noreply.github.com>
2025-04-02 10:56:25 -0500

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,7 +1,7 @@
 
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Jacob Oh / 001
  *
  *   This java file contains the problem solutions for the methods lastBoulder,
  *   showDuplicates, and pair methods. You should utilize the Java Collection
